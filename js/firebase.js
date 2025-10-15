d4bceeaa26978682ec0e5c338bf39d57e4a3c6b5
AV814 <137857152+AV814@users.noreply.github.com>
2025-10-14 20:12:11 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,14 +5,14 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-
 
 // Your Firebase configuration (replace with your own)
 const firebaseConfig = {
-  apiKey: "AIzaSyCFNJqBf9qarbQ4E8S040ahUqfFRGKjSmI",
-  authDomain: "projectcards-e2823.firebaseapp.com",
-  databaseURL: "https://projectcards-e2823-default-rtdb.firebaseio.com",
-  projectId: "projectcards-e2823",
-  storageBucket: "projectcards-e2823.appspot.com",
-  messagingSenderId: "813235068497",
-  appId: "1:813235068497:web:e31e228531a0a5c1a22270",
-  measurementId: "G-H1P4YKHBYR"
+  apiKey: "AIzaSyD4YBiTeK92QN_BkUSj9haPl_Cfb0CTgAg",
+  authDomain: "projectcards-b6b53.firebaseapp.com",
+  databaseURL: "https://projectcards-b6b53-default-rtdb.firebaseio.com",
+  projectId: "projectcards-b6b53",
+  storageBucket: "projectcards-b6b53.firebasestorage.app",
+  messagingSenderId: "625461773361",
+  appId: "1:625461773361:web:b47ae17ec449966a418110",
+  measurementId: "G-FV690XG2R7"
 };
 
 // Initialize Firebase
