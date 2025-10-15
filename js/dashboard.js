7a3d40411b26d0393ad453df29128841e39dd4d5
AV814 <137857152+AV814@users.noreply.github.com>
2025-10-14 20:17:04 -0400

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,7 +16,7 @@ onAuthStateChanged(auth, async (user) => {
 
     if (userData) {
       welcomeText.textContent = `Welcome, ${userData.username}!`;
-      pointsText.textContent = `Cash: $${userData.points}`;
+      pointsText.textContent = `Points: ${userData.points}`;
 
       // Show cards (empty if none)
       cardList.innerHTML = "";
